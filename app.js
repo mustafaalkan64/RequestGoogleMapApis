b94b930285cc111efa257316334638b5ad95747a
Mustafa ALKAN <mustafaalkan64@gmail.com>
2018-02-19 00:24:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 request({
 
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.2298,41.88754&key=AIzaSyC94KQTIk_2CJW-yTMrtjk7cSs-X2ikc-8',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.2298,41.88754&key=YOUR-API-KEY',
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(response, undefined, 2));
